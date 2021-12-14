b0acc28c080ea8c3296f99441f26ee0b9b0201a5
Johan Steven Arias Vasquez <yohan@Johans-MacBook-Pro-Personal.local>
2021-12-14 11:20:34 -0500

    Modules Creation

---
+++ instance/variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+  default = ""
+  description = "AMI Id"
+  type = string
+}
+
+variable "instance_type" {}
+
+variable "tags" {
+  type = map
+}
+
+variable "sg_name" {
+
+}
+
+variable "ingress_rules" {}
